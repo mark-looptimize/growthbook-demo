54371868e9448e2fda40f9b06e54edaa04522288
Mark Hoad <mark.hoad@looptimize.com>
2023-04-18 11:12:05 +0100

    Remove boilerplate cruft

--- src/growthbook-demo.ts
+++ src/growthbook-demo.ts
@@ -1,11 +1,11 @@
 import { LitElement, html, css } from 'lit';
 import { property, customElement } from 'lit/decorators.js';
 
-const logo = new URL('../../assets/open-wc-logo.svg', import.meta.url).href;
+const logo = new URL('../../assets/looptimize_logo.png', import.meta.url).href;
 
 @customElement('growthbook-demo')
 export class GrowthbookDemo extends LitElement {
-  @property({ type: String }) header = 'My app';
+  @property({ type: String }) header = 'Growthbook Demo';
 
   static styles = css`
     :host {
@@ -28,54 +28,15 @@ export class GrowthbookDemo extends LitElement {
 
     .logo {
       margin-top: 36px;
-      animation: app-logo-spin infinite 20s linear;
-    }
-
-    @keyframes app-logo-spin {
-      from {
-        transform: rotate(0deg);
-      }
-      to {
-        transform: rotate(360deg);
-      }
-    }
-
-    .app-footer {
-      font-size: calc(12px + 0.5vmin);
-      align-items: center;
-    }
-
-    .app-footer a {
-      margin-left: 5px;
     }
   `;
 
   render() {
     return html`
       <main>
-        <div class="logo"><img alt="open-wc logo" src=${logo} /></div>
+        <div class="logo"><img alt="open-wc logo" src=${logo} width="250"/></div>
         <h1>${this.header}</h1>
-
-        <p>Edit <code>src/GrowthbookDemo.ts</code> and save to reload.</p>
-        <a
-          class="app-link"
-          href="https://open-wc.org/guides/developing-components/code-examples"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Code examples
-        </a>
       </main>
-
-      <p class="app-footer">
-        🚽 Made with love by
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://github.com/open-wc"
-          >open-wc</a
-        >.
-      </p>
     `;
   }
 }
